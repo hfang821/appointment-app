633bc698540d087b848c61c022a93e8ce54a2036
Haoyu Fang <haoyufang@Haoyus-MacBook-Pro.local>
2024-08-13 16:28:03 -0400

    add a appointment component, a appointment interface for storing all the data models

---
+++ src/app/appointment-list/appointment-list.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Appointment } from '../models/appointment';
+
+@Component({
+  selector: 'app-appointment-list',
+  templateUrl: './appointment-list.component.html',
+  styleUrls: ['./appointment-list.component.css']
+})
+export class AppointmentListComponent {
+
+  appointment: Appointment = {
+    id: 1,
+    title: 'Take dog for a walk',
+    date: new Date('2023-07-30')
+  }
+
+}
+
+// When you don't need an object or method that contain actual values, use interfaces.
