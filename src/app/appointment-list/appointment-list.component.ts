804ede413cb88f529dad60db9e440d1b9ecec1e5
Kevin Fang <kevin-fang@hotmail.ca>
2024-08-14 13:34:11 -0400

    make the data persistent on the page using ngOnInit

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { Appointment } from '../models/appointment';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrls: ['./appointment-list.component.css']
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent implements OnInit {
+  ngOnInit(): void {
+    let savedAppointments = localStorage.getItem("appointments");
+    this.appointments = savedAppointments ? JSON.parse(savedAppointments) : [];
+  }
 
   newAppointmentTitle: string = "";
   newAppointmentDate: Date = new Date();
@@ -27,12 +32,15 @@ export class AppointmentListComponent {
       // Reset the form
       this.newAppointmentTitle = "";
       this.newAppointmentDate = new Date();
+
+      localStorage.setItem("appointments", JSON.stringify(this.appointments))
     }
   }
 
   deleteAppointment(index: number){
     // Remove one appointment from the list by given index
-    this.appointments.splice(index, 1)
+    this.appointments.splice(index, 1);
+    localStorage.setItem("appointments", JSON.stringify(this.appointments))
   }
 }
 
